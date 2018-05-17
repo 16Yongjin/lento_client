5a2d0114e02d8e20421f2dbaaebcf5e90742e760
16Yongjin <yongjin0802@gmail.com>
2018-05-18 01:35:16 +0900

    use chached map og-image

--- plugins/map-image.js
+++ plugins/map-image.js
@@ -1,4 +1,3 @@
-export default function ({ lat, lng }) {
-  return lat & lng ? `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=18&size=300x200&maptype=roadmap&markers=color:red%7C${lat},${lng}&key=AIzaSyDsT5FxGGcpWD6-Qq_Bh2PGCYl1fUPrS2Q`
-    : null
+export default function ({ _id, lat, lng }) {
+  return lat && lng ? `https://api.lento.in/public/maps/${_id.substr(-5)}.png` : null
 }
